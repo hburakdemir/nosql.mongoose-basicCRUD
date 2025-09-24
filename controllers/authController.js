d728fd77726c726aeacd8caca4e65686282dd048
Hakan Burak DEMİR <burakd279@gmail.com>
2025-09-24 20:03:19 +0300

    create token function on register added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,45 +1,60 @@
-// controllers/authController.js
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/authModel");
 const { validateEmail, validatePassword,emailZod } = require("../helpers/validation");
 
-// REGISTER
+
+//Register
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-
+  
   if (!name || !email || !password) {
     return res.status(400).json({ message: "All fields are required" });
   }
-
-  if (!emailZod(email)) {
+  
+  if (!validateEmail(email)) {
     return res
       .status(400)
       .json({ message: "Email must be: email@example.com" });
   }
-
+  
   if (!validatePassword(password)) {
     return res.status(400).json({
       message:
         "Password must have at least 8 characters, 1 uppercase, 1 number, 1 special character",
     });
   }
-
+  
   try {
-    const existingUser = await User.findOne({ email }); // Email kayıt olmuş mu kontrol
+    const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
-
+    
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
+    
     const newUser = await User.create({
       name,
       email,
       password: hashedPassword,
     });
 
-    res.status(201).json({ message: "User registered successfully" });
+    // kayıt olunca token oluşturuyoruz :D
+    const token = jwt.sign(
+      { userId: newUser._id, username: newUser.name, email: newUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({ 
+      message: "User registered successfully",
+      token: token,
+      user: { 
+        id: newUser._id, 
+        name: newUser.name, 
+        email: newUser.email 
+      }
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
